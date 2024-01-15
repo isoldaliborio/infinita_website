e2142a355814eafbe161df85daf3e5f2c29e14ef
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-15 15:34:14 +0000

    added initial header component files

---
+++ project_folder/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+'use client'
+import styles from './styles/Header.module.css'
+
+//TODO: make the best header ever!!!
+// Plan: 1. Header is comprised of three separate components-
+    // -Logo 
+    // -Navbar
+    // -LangSwitcher
+
+
+export default function Header(){
+    return <section id={styles.header}>placeholder</section>
+}
