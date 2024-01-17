a6aef5a73247d3e7ca2acea6951961928be39dd5
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-17 12:54:10 +0000

    building nav bar links

--- project_folder/src/components/Header/NavigationBar.tsx
+++ project_folder/src/components/Header/NavigationBar.tsx
@@ -1,32 +1,17 @@
 'use client';
 
+import Link from 'next/link';
+import styles from './styles/NavigationBar.module.css'
+
 export default function NavigationBar(){
 
-    const navBarItems = [
-        {
-          title: "home" ,
-          href: "/"
-        },
-        {
-          title: "about" ,
-          href: "/about"
-        },
-        {
-          title: "projects" ,
-          href: "/projects"
-        },
-        {
-          title: "services" ,
-          href: "/services"
-        },
-        {
-          title: "Contact" ,
-          href: "/contact"
-        },
-    ];
-
     return <>
-        placeholder Navbar
+        <Link
+        href='/about'
+        className={styles.navButton}
+        >
+            about
+        </Link>
     </>
 
 }
