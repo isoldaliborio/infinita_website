e707c8bfa9d165371a2248e72d9ed009ab93349e
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-17 10:53:15 +0000

    added some code to the Navigation Bar component, just a function returning a placeholder div

--- project_folder/src/components/Header/Header.tsx
+++ project_folder/src/components/Header/Header.tsx
@@ -5,15 +5,17 @@ import styles from './styles/Header.module.css'
 // Plan: 1. Header is comprised of three separate components-
     // -Logo 
     // -Navbar
-        //-will utilise getPathname from next navigation to style current route's button (might have to use regEx?)
     // -LangSwitcher
-        //-can use global state (e.g context) and local storage to save user's preference and implement across the whole site.
-//Header must be place at the top of the screen. This currently done with 'position:fixed' and 'top:0; left:0' to force the element
-//to the the top of the screen. However this solution means that elements can also sit beneath it. if so, we need to ensure proper padding
+//Header is placed at the top of the screen. This currently done with 'position:fixed' and 'top:0; left:0' to force the element
+//to the the top of the screen. However this solution means that elements can also sit beneath, and we will need to ensure proper padding
 //stops any content from being obscured, and that this works across screen sizes. An alternative would be to wrap the header and all other
 //elements in a flexbox, in the root Layout file.
 
+//Header is rendered in layout.tsx in the app folder, meaning it will persist across all page routes.
+
 
 export default function Header(){
-    return <section id={styles.header}>placeholder</section>
+    return <section id={styles.header}>
+        placeholder
+    </section>
 }
---
+++ project_folder/src/components/Header/NavigationBar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+export default function NavigationBar(){
+
+    const navBarItems = [
+        {
+          title: "home" ,
+          href: "/"
+        },
+        {
+          title: "about" ,
+          href: "/about"
+        },
+        {
+          title: "projects" ,
+          href: "/projects"
+        },
+        {
+          title: "services" ,
+          href: "/services"
+        },
+        {
+          title: "Contact" ,
+          href: "/contact"
+        },
+    ];
+
+    return <>
+        placeholder Navbar
+    </>
+
+}
