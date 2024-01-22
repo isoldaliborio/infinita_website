db377d852e9d1f3846e3d7a7199c1e529bcd262c
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-22 17:26:47 +0000

    minor change to import syntax

--- project_folder/src/components/Header/Header.tsx
+++ project_folder/src/components/Header/Header.tsx
@@ -4,16 +4,15 @@ import NavigationBar from './NavigationBar'
 import styles from './styles/Header.module.scss'
 import { useState } from 'react';
 import { LanguageContextType, LanguageContext } from '../Context/LanguageContext'
-
+ 
+export default function Header(){
     
-    export function Header(){
-        
-        const [language, setLanguage] = useState<LanguageContextType>("EN")
+    const [language, setLanguage] = useState<LanguageContextType>("EN")
 
-        return <LanguageContext.Provider value={ language }>
-            <div id={styles.header}>
-                logo
-                <NavigationBar setLanguage = {setLanguage}/>
-            </div>
-        </LanguageContext.Provider>
+    return <LanguageContext.Provider value={ language }>
+        <div id={styles.header}>
+            logo
+            <NavigationBar setLanguage = {setLanguage}/>
+        </div>
+    </LanguageContext.Provider>
 }
