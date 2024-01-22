12152e19aec8fecd12404ba691f816841d73945a
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-22 10:33:26 +0000

    navigation bar now renders each nav button by mapping through an array, reducing repetitive code

--- project_folder/src/components/Header/NavigationBar.tsx
+++ project_folder/src/components/Header/NavigationBar.tsx
@@ -8,59 +8,39 @@ export default function NavigationBar(){
 
     const getPathname = usePathname();
 
-    // const navButtonArray = [
-    //     {
-    //         pageName: 'home',
-    //         href: '/'
-    //     },
-    //     {
-    //         pageName: 'about',
-    //         href: '/about'
-    //     },
-    //     {
-    //         pageName: 'projects',
-    //         href: '/projects'
-    //     },
-    //     {
-    //         pageName: 'services',
-    //         href: '/services'
-    //     },
-    //     {
-    //         pageName: 'contact',
-    //         href: '/contact'
-    //     },
-    // ]
+    const navButtonArray = [
+        {
+            pageName: 'home',
+            href: '/'
+        },
+        {
+            pageName: 'about',
+            href: '/about'
+        },
+        {
+            pageName: 'projects',
+            href: '/projects'
+        },
+        {
+            pageName: 'services',
+            href: '/services'
+        },
+        {
+            pageName: 'contact',
+            href: '/contact'
+        },
+    ]
 
-    return <>
-        <Link
-        href='/'
-        className={`${styles.navButton} ${getPathname === '/' ? styles.activeButton : ''}`}
-        >
-            home
-        </Link>
-        <Link
-        href='/about'
-        className={`${styles.navButton} ${getPathname === '/about' ? styles.activeButton : ''}`}
-        >
-            about
-        </Link>
-        <Link
-        href='/projects'
-        className={`${styles.navButton} ${getPathname === '/projects' ? styles.activeButton : ''}`}
-        >
-            projects
-        </Link>
-        <Link
-        href='/services'
-        className={`${styles.navButton} ${getPathname === '/services' ? styles.activeButton : ''}`}
-        >
-            services
-        </Link>
-        <Link
-        href='/contact'
-        className={`${styles.navButton} ${getPathname === '/contact' ? styles.activeButton : ''}`}
-        >
-            contact
-        </Link>
-</>
+    return (<>
+        {navButtonArray.map((item) => (
+            <Link
+            href={item.href}
+            key={item.pageName}
+            className={`${styles.navButton} ${getPathname === item.href ? styles.activeButton : ''}`}
+            >
+                {item.pageName}
+            </Link>
+        ))}
+    </>
+    ) 
 }
