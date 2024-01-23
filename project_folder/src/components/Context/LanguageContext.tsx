bc5152a2816545ef081696a2e50ee10edecabf8c
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-23 10:26:11 +0000

    added comments to context file

--- project_folder/src/components/Context/LanguageContext.tsx
+++ project_folder/src/components/Context/LanguageContext.tsx
@@ -2,6 +2,8 @@
 
 import { createContext } from "react";
 
+//Import and use this when defining types of state and setState related to this context.
 export type LanguageContextType = "EN" | "PT";
 
+//Import into files that need to read this context.
 export const LanguageContext = createContext<LanguageContextType>("EN");
