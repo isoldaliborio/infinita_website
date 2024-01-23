9270f35bba9222879578c69a9674b9206565e728
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-23 17:44:42 +0000

    google font added to navbar, some transitions added too

--- project_folder/src/components/Context/LanguageContext.tsx
+++ project_folder/src/components/Context/LanguageContext.tsx
@@ -5,5 +5,5 @@ import { createContext } from "react";
 //Import and use this when defining types of state and setState related to this context.
 export type LanguageContextType = "EN" | "PT";
 
-//Import into files that need to read this context.
+//Import into files that need to use this context.
 export const LanguageContext = createContext<LanguageContextType>("EN");
