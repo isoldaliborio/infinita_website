fbf2b81b167ee35e07c0bd96eb3a833edd4a06ca
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-24 11:32:56 +0000

    added a new component, main, that wraps all other components in a context provider, allowing context to be read across all components- this will let us change the language using the header and update the text on all pages

--- project_folder/src/app/about/page.tsx
+++ project_folder/src/app/about/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import styles from './page.module.scss'
+import { LanguageContext } from '@/components/Context/LanguageContext'
+import { useContext } from 'react';
 
 export default function About() {
+
+  let language = useContext(LanguageContext);
+
     return (
       <main className={styles.main}>
         <div className={styles.description}>
-          Welcome to the about page!
+          {language === 'EN' ? 'Welcome to the about page!' : 'Bem-vindo à página sobre' }
         </div>
       </main>
     )
--- project_folder/src/components/Header/Header.tsx
+++ project_folder/src/components/Header/Header.tsx
@@ -2,18 +2,18 @@
 
 import styles from "./styles/Header.module.scss"
 import  Image  from "next/Image";
-import { useState } from "react";
-import { LanguageContextType, LanguageContext } from "../Context/LanguageContext";
+import { LanguageContextType } from "../Context/LanguageContext";
 import NavigationBar from "./NavigationBar";
- 
-export default function Header(){
-    
-    const [language, setLanguage] = useState<LanguageContextType>("EN")
 
-    return <LanguageContext.Provider value={ language }>
-        <div id={styles.header}>
-            <Image id={styles.infinitaLogo} src="/images/infinita-logo.png" alt="infinita logo" width="200" height="50"/>
-            <NavigationBar setLanguage = {setLanguage}/>
-        </div>
-    </LanguageContext.Provider>
+interface HeaderProps{
+    setLanguage: React.Dispatch<React.SetStateAction<LanguageContextType>>;
 }
+ 
+export default function Header({setLanguage}:HeaderProps){
+
+    return <div id={styles.header}>
+        <Image id={styles.infinitaLogo} src="/images/infinita-logo.png" alt="infinita logo" width="200" height="50"/>
+        <NavigationBar setLanguage = {setLanguage}/>
+    </div>
+
+};
