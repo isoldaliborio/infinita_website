fb5d42ed7f5626d624ad5ccee3da30bf2f64b73e
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-29 13:57:20 +0000

    added useContext to each page to read language context and update page text accordingly

--- project_folder/src/app/projects/page.tsx
+++ project_folder/src/app/projects/page.tsx
@@ -1,11 +1,18 @@
-import styles from './page.module.scss'
+"use client";
+
+import styles from "./page.module.scss";
+import { LanguageContext } from "@/components/Context/LanguageContext";
+import { useContext } from "react";
 
 export default function Projects() {
-    return (
-      <main className={styles.main}>
-        <div className={styles.description}>
-          Welcome to the projects page!
-        </div>
-      </main>
-    )
+
+  let language = useContext(LanguageContext);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        {language === "EN" ? "Welcome to the project page!" : "Bem-vindo à página projetos" }
+      </div>
+    </main>
+  )
   }
