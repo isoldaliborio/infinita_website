0cc013535a97208518c026efae37b19d70e50485
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-01-31 13:50:46 +0000

    Fetching home page from wordpress

--- project_folder/src/components/Main/Main.tsx
+++ project_folder/src/components/Main/Main.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { LanguageContextType, LanguageContext } from "../Context/LanguageContext";
+import processHomePageData from "@/utils/processHomePageData"
 
 export default function Main({children}:{children: React.ReactNode}){
+
+    useEffect(() => {
+        processHomePageData();
+    }, []);
 
     const [language, setLanguage] = useState<LanguageContextType>("EN")
 
