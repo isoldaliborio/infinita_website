bd2975c64f34b3e9f4f8d3923f280f345caabd69
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-02-01 18:18:56 +0000

    Using fonts from google

--- project_folder/next.config.js
+++ project_folder/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 protocol: 'https',
                 hostname:  'infinitaproductions.com',
                 port: '',
-                pathname: '/admin/wp-content/**'
+                pathname: '/**'
             }
         ]
     }
