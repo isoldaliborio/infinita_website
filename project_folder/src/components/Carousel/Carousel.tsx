bb06dfe10e2af7a1f58ed85d6bbda5d86cd7e89f
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-02-04 16:38:43 +0000

    Moved data fetch to carousel

--- project_folder/src/components/Carousel/Carousel.tsx
+++ project_folder/src/components/Carousel/Carousel.tsx
@@ -1,30 +1,31 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import styles from "./styles/Carousel.module.scss"
 import Buttons from "@/components/Carousel/Buttons"
 import Image from "next/image"
-import { CarouselDataContext } from "../../context/CarouselDataContext";
-import { useState, useContext } from "react";
+import processHomePageData from "@/utils/processHomePageData";
 import LoadingScreen from "../Loading/LoadingScreen";
 
 export default function Carousel(){
 
-    const fetchedData: any = useContext(CarouselDataContext)
+    const [homePageData, setHomePageData] = useState<any>();
+    const [activeItem, setActiveItem] = useState<any>();
+
+    useEffect(() => {
+        const fetchData = async () => setHomePageData(await processHomePageData());
+        fetchData().catch(console.error);
+    }, []);
+
+    useEffect(() => {homePageData && setActiveItem(homePageData[0])}, [homePageData]);
     
-    //rather than checking activeItem, we check fetchedData. meaning it will only load once the context
-    //is no longer null.
-    if(!fetchedData){
+    if(!homePageData || !activeItem){
         return <LoadingScreen/>
     } 
     
-    //if this was above the if statement, it would throw an error trying to assign [0] to a null value
-    //here, the state will only be created after the data has been fetched, removing any errors.
-    const [activeItem, setActiveItem] = useState<any>(fetchedData[0]);
-
-
     return (
         <section id={styles.Carousel}>
-            <Buttons data={fetchedData} setItem={setActiveItem} activeItem={activeItem} />
+            <Buttons data={homePageData} setItem={setActiveItem} activeItem={activeItem} />
             <div className={styles.imageContainer}>
                 <Image id={styles.imagespace} src={activeItem.img_url} alt="img" style={{objectFit: "cover"}} fill priority={true} />
             </div>
