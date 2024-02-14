aecf529a7b008056ea3521987fa51d8a59750ed2
isoldaliborio <isoldaliborio@gmail.com>
2024-02-14 12:27:48 +0000

    adjustment space and size buttons

--- project_folder/src/components/Carousel/Buttons.tsx
+++ project_folder/src/components/Carousel/Buttons.tsx
@@ -25,38 +25,38 @@ export default function Buttons({ data, setItem, activeItem, isLoading, setIsVis
 
   var imageCycleIndex = 0;
 
-    // cycle through images
-    useEffect(() => {
-      const cycleInterval = setInterval(() => {
-        if(!isLoading && isCycling){
-          setIsVisible(false); //triggers image fade-out
-          console.log(isCycling);
-          if(imageCycleIndex >= 3){
-            imageCycleIndex = 0;
-          } else{
-            imageCycleIndex++;
-          }
-          data[imageCycleIndex].index = imageCycleIndex
-          setItem(data[imageCycleIndex], imageCycleIndex)
-        }
-      }, 5000)
-        return () => clearInterval(cycleInterval);
-    }, [isCycling]);
+    // // cycle through images
+    // useEffect(() => {
+    //   const cycleInterval = setInterval(() => {
+    //     if(!isLoading && isCycling){
+    //       setIsVisible(false); //triggers image fade-out
+    //       console.log(isCycling);
+    //       if(imageCycleIndex >= 3){
+    //         imageCycleIndex = 0;
+    //       } else{
+    //         imageCycleIndex++;
+    //       }
+    //       data[imageCycleIndex].index = imageCycleIndex
+    //       setItem(data[imageCycleIndex], imageCycleIndex)
+    //     }
+    //   }, 5000)
+    //     return () => clearInterval(cycleInterval);
+    // }, [isCycling]);
 
-    //reset cycling after user has clicked on a button
-    useEffect(() => {
-      const cycleInterval = setInterval(() => {
-        console.log('checking to see if cycling...')
-        if(!isLoading && !isCycling){
-          console.log('is not cycling, resetting cycle...')
-          setIsCycling(true);
-          return () => clearInterval(cycleInterval);
-        } else{
-          console.log('is cycling, doing nothing...')
-        }
-      }, 15000)
-        return () => clearInterval(cycleInterval);
-    }, [isCycling]);
+    // //reset cycling after user has clicked on a button
+    // useEffect(() => {
+    //   const cycleInterval = setInterval(() => {
+    //     console.log('checking to see if cycling...')
+    //     if(!isLoading && !isCycling){
+    //       console.log('is not cycling, resetting cycle...')
+    //       setIsCycling(true);
+    //       return () => clearInterval(cycleInterval);
+    //     } else{
+    //       console.log('is cycling, doing nothing...')
+    //     }
+    //   }, 15000)
+    //     return () => clearInterval(cycleInterval);
+    // }, [isCycling]);
       
     return (
         <div className={styles.button_block}>
