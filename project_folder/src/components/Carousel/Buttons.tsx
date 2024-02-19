ec7342bf0c08f6f251d29fc37b3cde87bed52e67
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-02-19 23:28:46 +0000

    Animation fixes

--- project_folder/src/components/Carousel/Buttons.tsx
+++ project_folder/src/components/Carousel/Buttons.tsx
@@ -1,76 +1,115 @@
 import { motion } from "framer-motion";
 import styles from "./styles/Buttons.module.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+
 
 export default function Buttons({ data, setItem, activeItem, isLoading, setIsVisible }: { data: any; setItem: any; activeItem: any; isLoading: boolean; setIsVisible: any }) {
 
   const [isCycling, setIsCycling] = useState(true);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [buttonWidth, setButtonWidth] = useState(null);
+  const buttonRef = useRef(null);
 
+
+  // Detect screen width
+  useEffect(() => {
+    const updateScreenWidth = () => setScreenWidth(window.innerWidth);
+    updateScreenWidth();
+    window.addEventListener('resize', updateScreenWidth);
+    return () => window.removeEventListener('resize', updateScreenWidth);
+  }, []);
+
+  // Detect button width
+  useEffect(() => {
+    const updateButtonWidth = () => {
+      if (buttonRef.current) {
+        const currentWidth = buttonRef.current.offsetWidth;
+        setButtonWidth(currentWidth);
+      }
+    };
+    updateButtonWidth();
+    window.addEventListener('resize', updateButtonWidth);
+    return () => window.removeEventListener('resize', updateButtonWidth);
+  }, []);
+
+  // Click handler
   const handleClick = (item: any, index: number) => {
     if (!isLoading) {
       item.index = index;
       setItem(item, index);
     }
   };
-  
+
+  // Animation config
   const variants = {
-    closed: { width:"40px"},
-    open: { width:"fit-content", maxWidth:"fit-content", },
+    closed: {
+      width: buttonWidth,
+      // Desktop-specific config (i.e. free height on mobile)
+      ...(screenWidth > 700 && { height: buttonWidth }),
+    },
+    open: {
+      width: "fit-content",
+      // Desktop-specific config
+      ...(screenWidth > 700 && { height: buttonWidth, maxWidth: "fit-content" }),
+    },
+  };
+
+  const transition = {
+    duration: 0.6
   }
 
+  // Image cycling
   var imageCycleIndex = 0;
 
-    // cycle through images
-    useEffect(() => {
-      const cycleInterval = setInterval(() => {
-        if(!isLoading && isCycling){
-          setIsVisible(false); //triggers image fade-out
-          console.log(isCycling);
-          if(imageCycleIndex >= 3){
-            imageCycleIndex = 0;
-          } else{
-            imageCycleIndex++;
-          }
-          data[imageCycleIndex].index = imageCycleIndex
-          setItem(data[imageCycleIndex], imageCycleIndex)
+  useEffect(() => {
+    const cycleInterval = setInterval(() => {
+      if (!isLoading && isCycling) {
+        setIsVisible(false); //triggers image fade-out
+        console.log(isCycling);
+        if (imageCycleIndex >= 3) {
+          imageCycleIndex = 0;
+        } else {
+          imageCycleIndex++;
         }
-      }, 5000)
-        return () => clearInterval(cycleInterval);
-    }, [isCycling]);
+        data[imageCycleIndex].index = imageCycleIndex
+        setItem(data[imageCycleIndex], imageCycleIndex)
+      }
+    }, 5000)
+    return () => clearInterval(cycleInterval);
+  }, [isCycling]);
 
-    //reset cycling after user has clicked on a button
-    useEffect(() => {
-      const cycleInterval = setInterval(() => {
-        console.log('checking to see if cycling...')
-        if(!isLoading && !isCycling){
-          console.log('is not cycling, resetting cycle...')
-          setIsCycling(true);
-          return () => clearInterval(cycleInterval);
-        } else{
-          console.log('is cycling, doing nothing...')
-        }
-      }, 15000)
+  // Reset cycling on click
+  useEffect(() => {
+    const cycleInterval = setInterval(() => {
+      console.log('checking to see if cycling...')
+      if (!isLoading && !isCycling) {
+        console.log('is not cycling, resetting cycle...')
+        setIsCycling(true);
         return () => clearInterval(cycleInterval);
-    }, [isCycling]);
-      
-    return (
-        <div className={styles.button_block}>
-            {/* Iterate through the "featured" array and create Icon components */}
-            {data.map((item: any, index: any) => (
-                <motion.div 
-                    animate={activeItem.title == item.title && !isLoading ? "open" : "closed"}
-                    variants={variants}
-                    transition={{duration:0.6}}
-                    key={index} 
-                    className={`${styles.button} ${activeItem.img_url == item.img_url && !isLoading && activeItem.title ? styles.active_button : ""}`}
-                    onClick={() => handleClick(item, index)}
-                >
-                    <div id={styles.index}>0{index + 1}</div>
-                    <span id={styles.title}>{item.title}</span> 
+      } else {
+        console.log('is cycling, doing nothing...')
+      }
+    }, 15000)
+    return () => clearInterval(cycleInterval);
+  }, [isCycling]);
 
-                    <span id={styles.category}> {item.category_names[0]}</span>
-                </motion.div>
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.button_block}>
+      {data.map((item: any, index: any) => (
+        <motion.div
+          animate={activeItem.title == item.title && !isLoading ? "open" : "closed"}
+          variants={variants}
+          transition={transition}
+          key={index}
+          ref={buttonRef}
+          className={`${styles.button} ${activeItem.img_url == item.img_url && !isLoading && activeItem.title ? styles.active_button : ""}`}
+          onClick={() => handleClick(item, index)}
+        >
+          <div className={styles.index}>0{index + 1}</div>
+          <span className={styles.title}>{item.title}</span>
+          <span className={styles.category}> {item.category_names[0]}</span>
+        </motion.div>
+      ))}
+    </div>
+  );
 };
