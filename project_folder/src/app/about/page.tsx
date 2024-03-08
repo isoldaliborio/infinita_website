303f4717bcb373949260baac2f4c67f8a35a837f
isoldaliborio <isoldaliborio@gmail.com>
2024-03-08 19:46:39 +0000

    create grid classes in global - teste in about page

--- project_folder/src/app/about/page.tsx
+++ project_folder/src/app/about/page.tsx
@@ -9,10 +9,23 @@ export default function About() {
   let language = useContext(LanguageContext);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        {language === 'EN' ? 'Welcome to the about page!' : 'Bem-vindo à página sobre' }
-      </div>
+    <main className={`${styles.main} marginL marginR`}>
+      <section className={styles.aboutBox}>
+        <div className={`${styles.textAbout} col-5`}>
+          <p className={styles.title}>
+            {language === 'EN' ? 'Infinita Production' : 'Infinita Produções' }
+          </p>
+          <p className={styles.description}>
+            {language === 'EN' ? 'Welcome to the about page!' : 'Bem-vindo à página sobre' }
+          </p>
+        </div>
+        <div className={`${styles.categoryBox} col-5`}>
+          categories
+        </div>
+      </section>
+      <section className={`${styles.imageBox} col-7`}>
+          Imagem
+      </section>
     </main>
   )
   }
