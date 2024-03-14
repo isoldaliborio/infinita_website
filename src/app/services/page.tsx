b56b03b724b9c4056122be803381dd0e20465ae7
NoahAldhous <naldhous@hotmail.co.uk>
2024-03-14 14:17:54 +0000

    initial implementation of accordion component

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -5,9 +5,16 @@ import { LanguageContext } from "@/context/LanguageContext";
 import { useContext } from "react";
 import TitleBanner from "@/components/TitleBanner/TitleBanner";
 import { useState, useEffect } from "react";
-import { getServicesPageDataJson, getImageUrl } from '@/lib/processServicesData';
+import { getServicesPageDataJson} from '@/lib/processServicesData';
 import Image from 'next/image';
 import LoadingScreen from "@/components/Loading/LoadingScreen";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 
 export default function Services() {
@@ -36,10 +43,39 @@ export default function Services() {
               { language === "EN" ? servicesData.service_main_text_en : servicesData.service_main_text_pt}
             </section>
             <section className={styles.imageBox}>
-              placeholder image
-              <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_Brazil.jpg"} alt={"img test"} width={100} height={100}/>
-              <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_UK.jpg"} alt={"img test"} width={100} height={100}/>
-              <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_Europe.jpg"} alt={"img test"} width={100} height={100}/>
+              <Accordion type="single" collapsible>
+
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className={styles.accordionTitle}>
+                    Brazil
+                    <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_Brazil.jpg"} alt={"img test"} width={100} height={100}/>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    { language === "EN" ? servicesData.brazil_service_text_en : servicesData.brazil_service_text_pt } 
+                  </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-2">
+                  <AccordionTrigger className={styles.accordionTitle}>
+                    UK
+                    <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_UK.jpg"} alt={"img test"} width={100} height={100}/>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                  { language === "EN" ? servicesData.uk_service_text_en : servicesData.uk_service_text_pt } 
+                  </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-3">
+                  <AccordionTrigger className={styles.accordionTitle}>
+                    Europe
+                    <Image src={"https://infinitaproducoes.com/wordpress/wp-content/uploads/2024/03/img_Services_Europe.jpg"} alt={"img test"} width={100} height={100}/>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                  { language === "EN" ? servicesData.europe_service_text_en : servicesData.europe_service_text_pt } 
+                  </AccordionContent>
+                </AccordionItem>
+
+              </Accordion>
             </section>
           </> 
         }
