4ed84c42d5a707c31e151b61818167469ba148f6
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-03-16 15:22:39 +0000

    Fixed broken routes on static export

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,25 +2,25 @@ import styles from "./styles/ContactForm.module.scss";
 
 export default function ContactForm() {
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      const formData = new FormData(event.target);
-      const inputName = formData.get('inputName');
-      const inputEmail = formData.get('inputEmail');
-      console.log(inputName, inputEmail) 
-  
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const inputName = formData.get('inputName');
+    const inputEmail = formData.get('inputEmail');
+    console.log(inputName, inputEmail)
+
     //   sendInputValueToApi(inputValue).then(() => /* Do something */)  
-    };
-  
-    return (
-      <form className={`${styles.form} col-5`} onSubmit={handleSubmit}>
-        <input className={`${styles.name} ${styles.input} `}type="string" name="inputName" placeholder="NAME">
-        </input>
-        <input className={`${styles.email} ${styles.input}`} type="string" name="inputEmail" placeholder="EMAIL"/>
-        <input className={`${styles.message} ${styles.input}`} type="string" name="message" placeholder="MESSAGE" />
-        <button className={`${styles.sendButton} col-2`} type="submit">SEND</button>
-      </form>
-    );
-  }
+  };
+
+  return (
+    <form className={`${styles.form} col-5`} onSubmit={handleSubmit}>
+      <input className={`${styles.name} ${styles.input} `} type="string" name="inputName" placeholder="NAME">
+      </input>
+      <input className={`${styles.email} ${styles.input}`} type="string" name="inputEmail" placeholder="EMAIL" />
+      <input className={`${styles.message} ${styles.input}`} type="string" name="message" placeholder="MESSAGE" />
+      <button className={`${styles.sendButton} col-2`} type="submit">SEND</button>
+    </form>
+  );
+}
 
