66e0c1ae2f8c220d26046e104c98945cfe9f365d
NoahAldhous <naldhous@hotmail.co.uk>
2024-03-20 11:08:32 +0000

    overlay created with gradient that appears on hover of grid elements

---
+++ src/lib/dummyProjectData.js
@@ -0,0 +1,255 @@
+
+export const dummyData = [
+    {
+      title: "1",
+      size: "2"
+    },
+    {
+      title: "2",
+      size: "2"
+    },
+    {
+      title: "3",
+      size: "1"
+    },
+    {
+      title: "4",
+      size: "1"
+    },
+    {
+      title: "5",
+      size: "2"
+    },
+    {
+      title: "6",
+      size: "2"
+    },
+    {
+      title: "7",
+      size: "3"
+    },
+    {
+      title: "8",
+      size: "1"
+    },
+    {
+      title: "9",
+      size: "2"
+    },
+    {
+      title: "10",
+      size: "1"
+    },
+    {
+      title: "11",
+      size: "2"
+    },
+    {
+      title: "12",
+      size: "1"
+    },
+    {
+      title: "13",
+      size: "1"
+    },
+    {
+      title: "14",
+      size: "2"
+    },
+    {
+      title: "15",
+      size: "1"
+    },
+    {
+      title: "16",
+      size: "2"
+    },
+    {
+      title: "17",
+      size: "2"
+    },
+    {
+      title: "18",
+      size: "3"
+    },
+    {
+      title: "19",
+      size: "1"
+    },
+    {
+      title: "20",
+      size: "3"
+    },
+    {
+      title: "21",
+      size: "2"
+    },
+    {
+      title: "22",
+      size: "3"
+    },
+    {
+      title: "23",
+      size: "2"
+    },
+    {
+      title: "24",
+      size: "3"
+    },
+    {
+      title: "25",
+      size: "1"
+    },
+    {
+      title: "26",
+      size: "1"
+    },
+    {
+      title: "27",
+      size: "2"
+    },
+    {
+      title: "28",
+      size: "3"
+    },
+    {
+      title: "29",
+      size: "1"
+    },
+    {
+      title: "30",
+      size: "2"
+    },
+    {
+      title: "31",
+      size: "1"
+    },
+    {
+      title: "32",
+      size: "2"
+    },
+    {
+      title: "33",
+      size: "1"
+    },
+    {
+      title: "34",
+      size: "1"
+    },
+    {
+      title: "35",
+      size: "2"
+    },
+    {
+      title: "36",
+      size: "1"
+    },
+    {
+      title: "37",
+      size: "2"
+    },
+    {
+      title: "38",
+      size: "2"
+    },
+    {
+      title: "39",
+      size: "3"
+    },
+    {
+      title: "40",
+      size: "1"
+    },
+    {
+      title: "41",
+      size: "3"
+    },
+    {
+      title: "42",
+      size: "2"
+    },
+    {
+      title: "43",
+      size: "3"
+    },
+    {
+      title: "44",
+      size: "2"
+    },
+    {
+      title: "45",
+      size: "3"
+    },
+    {
+      title: "46",
+      size: "1"
+    },
+    {
+      title: "47",
+      size: "1"
+    },
+    {
+      title: "48",
+      size: "2"
+    },
+    {
+      title: "49",
+      size: "3"
+    },
+    {
+      title: "50",
+      size: "1"
+    },
+    {
+      title: "51",
+      size: "2"
+    },
+    {
+      title: "52",
+      size: "1"
+    },
+    {
+      title: "53",
+      size: "2"
+    },
+    {
+      title: "54",
+      size: "1"
+    },
+    {
+      title: "55",
+      size: "1"
+    },
+    {
+      title: "56",
+      size: "2"
+    },
+    {
+      title: "57",
+      size: "1"
+    },
+    {
+      title: "58",
+      size: "2"
+    },
+    {
+      title: "59",
+      size: "2"
+    },
+    {
+      title: "60",
+      size: "3"
+    },
+    {
+      title: "61",
+      size: "1"
+    },
+    {
+      title: "62",
+      size: "3"
+    },
+    {
+      title: "63",
+      size: "2"
+    },
+  ]
