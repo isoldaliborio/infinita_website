9e94110860038e5ec3620de8f3339923cb944d7d
NoahAldhous <naldhous@hotmail.co.uk>
2024-03-20 10:52:05 +0000

    unfiltered semi-responsive masonry grid working using dummy data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,12 +10,277 @@ export default function Projects() {
 
   let language = useContext(LanguageContext);
 
+  const imageArray = [
+    {
+      title: "1",
+      size: "2"
+    },
+    {
+      title: "2",
+      size: "2"
+    },
+    {
+      title: "3",
+      size: "1"
+    },
+    {
+      title: "4",
+      size: "1"
+    },
+    {
+      title: "5",
+      size: "2"
+    },
+    {
+      title: "6",
+      size: "2"
+    },
+    {
+      title: "7",
+      size: "3"
+    },
+    {
+      title: "8",
+      size: "1"
+    },
+    {
+      title: "9",
+      size: "2"
+    },
+    {
+      title: "10",
+      size: "1"
+    },
+    {
+      title: "11",
+      size: "2"
+    },
+    {
+      title: "12",
+      size: "1"
+    },
+    {
+      title: "13",
+      size: "1"
+    },
+    {
+      title: "14",
+      size: "2"
+    },
+    {
+      title: "15",
+      size: "1"
+    },
+    {
+      title: "16",
+      size: "2"
+    },
+    {
+      title: "17",
+      size: "2"
+    },
+    {
+      title: "18",
+      size: "3"
+    },
+    {
+      title: "19",
+      size: "1"
+    },
+    {
+      title: "20",
+      size: "3"
+    },
+    {
+      title: "21",
+      size: "2"
+    },
+    {
+      title: "22",
+      size: "3"
+    },
+    {
+      title: "23",
+      size: "2"
+    },
+    {
+      title: "24",
+      size: "3"
+    },
+    {
+      title: "25",
+      size: "1"
+    },
+    {
+      title: "26",
+      size: "1"
+    },
+    {
+      title: "27",
+      size: "2"
+    },
+    {
+      title: "28",
+      size: "3"
+    },
+    {
+      title: "29",
+      size: "1"
+    },
+    {
+      title: "30",
+      size: "2"
+    },
+    {
+      title: "31",
+      size: "1"
+    },
+    {
+      title: "32",
+      size: "2"
+    },
+    {
+      title: "33",
+      size: "1"
+    },
+    {
+      title: "34",
+      size: "1"
+    },
+    {
+      title: "35",
+      size: "2"
+    },
+    {
+      title: "36",
+      size: "1"
+    },
+    {
+      title: "37",
+      size: "2"
+    },
+    {
+      title: "38",
+      size: "2"
+    },
+    {
+      title: "39",
+      size: "3"
+    },
+    {
+      title: "40",
+      size: "1"
+    },
+    {
+      title: "41",
+      size: "3"
+    },
+    {
+      title: "42",
+      size: "2"
+    },
+    {
+      title: "43",
+      size: "3"
+    },
+    {
+      title: "44",
+      size: "2"
+    },
+    {
+      title: "45",
+      size: "3"
+    },
+    {
+      title: "46",
+      size: "1"
+    },
+    {
+      title: "47",
+      size: "1"
+    },
+    {
+      title: "48",
+      size: "2"
+    },
+    {
+      title: "49",
+      size: "3"
+    },
+    {
+      title: "50",
+      size: "1"
+    },
+    {
+      title: "51",
+      size: "2"
+    },
+    {
+      title: "52",
+      size: "1"
+    },
+    {
+      title: "53",
+      size: "2"
+    },
+    {
+      title: "54",
+      size: "1"
+    },
+    {
+      title: "55",
+      size: "1"
+    },
+    {
+      title: "56",
+      size: "2"
+    },
+    {
+      title: "57",
+      size: "1"
+    },
+    {
+      title: "58",
+      size: "2"
+    },
+    {
+      title: "59",
+      size: "2"
+    },
+    {
+      title: "60",
+      size: "3"
+    },
+    {
+      title: "61",
+      size: "1"
+    },
+    {
+      title: "62",
+      size: "3"
+    },
+    {
+      title: "63",
+      size: "2"
+    },
+  ]
+
   return (
     <>
       <TitleBanner title={language === "EN" ? "PROJECTS" : "PROJETOS"} />
       <main className={styles.main}>
         <div className={styles.gridContainer}>
-          <div className={`${styles.firstChild} ${styles.gridChild}`}>
+          {
+            imageArray.map((item, index) => {
+              return <div key={index} className={`${styles.gridChild} ${item.size === "2" ? styles.horizontalPoster : ""} ${item.size === "3" ? styles.verticalPoster : ""}`}> 
+                {item.title}
+              </div>
+            })
+          }
+
+
+
+          {/* <div className={`${styles.firstChild} ${styles.gridChild}`}>
             1
             <Image className={styles.gridImage} src={"https://images.unsplash.com/photo-1710302112350-e14ceae0c385?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="unsplash image" height={100} width={100}/>
           </div>
@@ -81,7 +346,7 @@ export default function Projects() {
           </div>
           <div className={styles.twentyFirstChild}>
             21
-          </div>
+          </div> */}
         </div>
       </main>
     </>
