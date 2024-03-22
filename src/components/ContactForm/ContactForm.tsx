295ab6decec5806c72f99e703af986618ad1a5c6
isoldaliborio <isoldaliborio@gmail.com>
2024-03-22 19:21:41 +0000

    language placeholder contact - adjust padding main

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,8 +2,12 @@
 
 import styles from "./styles/ContactForm.module.scss";
 import { useState, useEffect } from "react";
+import { LanguageContext } from "@/context/LanguageContext";
+import { useContext } from "react";
 
 export default function ContactForm() {
+
+  let language = useContext(LanguageContext);
 
   // Workaround for LastPass extension. May be removed if we render the page based on some other state
   const [isClient, setIsClient] = useState(false);
@@ -22,9 +26,9 @@ export default function ContactForm() {
 
   return (
     <form className={`${styles.form} col-5`} onSubmit={handleSubmit} suppressHydrationWarning={true}>
-      <input className={`${styles.name} ${styles.input}`} type="string" name="inputName" placeholder="name" />
-      <input className={`${styles.email} ${styles.input}`} type="string" name="inputEmail" placeholder="email" />
-      <textarea className={`${styles.message} ${styles.input}`} type="string" name="message" placeholder="message" />
+      <input className={`${styles.name} ${styles.input}`} type="string" name="inputName" placeholder={language === "en" ? "name" : "Nome"} />
+      <input className={`${styles.email} ${styles.input}`} type="string" name="inputEmail" placeholder={language === "en" ? "email" : "email"} />
+      <textarea className={`${styles.message} ${styles.input}`} type="string" name="message" placeholder={language === "en" ? "message" : "mensagem"} />
       <button className={`${styles.submitButton} col-2`} type="submit">submit</button>
     </form>
   );
