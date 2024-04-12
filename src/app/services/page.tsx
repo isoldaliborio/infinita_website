6724ec1e5c034c4d1b6cbc66dcadde12c8b20dd2
NoahAldhous <naldhous@hotmail.co.uk>
2024-04-12 12:09:17 +0100

    styling for services mobile layout

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -37,7 +37,13 @@ export default function Services() {
       <main className={styles.main}>
         {!servicesData ? <LoadingScreen /> : <>
           <section className={styles.servicesText}>
-            {servicesData[`service_main_text_${language}`]}
+            {/* {servicesData[`service_main_text_${language}`]} */}
+            Infinita Productions is a production company based in London, UK and Salvador, Brazil. We aim to support projects in both countries, encouraging co-productions and stimulating a cultural dialogue. infinita is also committed to creating original content. <br/>
+            Our action areas are Films, Music, Curating and Accounting. <br/>
+            We can bring together a Brazilian dream team of professional filmmakers for your British production in Brazil and vice-versa. <br/>
+            We work with highly respected international producers in both a servicing and co-producing capacity while remaining dedicated to fostering Brazilian and British talents. (meet our team)
+            Infinita also strives to take an eco sustainable aoproach to all of its productions and donates a portion of its profits to social causes. By investing in Infinita you`re also supporting the environment.
+            We work with a team of experienced professionals to develop and deliver best results according to our clients needs.
           </section>
           <section className={styles.accordionContainer}>
             <ServicesAccordion
