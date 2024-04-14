54894794b91bf7f11f3c2056d3f9b5a411b8d35f
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-04-14 17:15:27 +0100

    Carousel implementation with external lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from './page.module.scss'
-import Carousel_v2 from '@/components/Carousel/Carousel_v2'
+import Carousel from '@/components/Carousel/Carousel'
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Carousel_v2 />
+      <Carousel />
     </main>
   )
 }
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,108 +1,67 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
-import Image from "next/image";
+import { useState, useEffect } from "react";
 import styles from "./styles/Carousel.module.scss";
 import Buttons from "@/components/Carousel/Buttons";
-import LoadingScreen from "../Loading/LoadingScreen";
-import { getHomePageDataJson, orderImages, processHomePageData } from "@/lib/processHomePageData";
-import { motion, AnimatePresence } from "framer-motion";
-import CarouselImage from "./CarouselImage";
+import { getHomePageDataJson, orderImages } from "@/lib/processHomePageData";
+
+import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.css"
+import "./styles/react-responsive-carousel-custom.css"
 
 export default function Carousel() {
-  const initialData = Array.from({ length: 4 }, () => ({
-    img_id: null,
-    work_id: null,
-    img_url: null,
-    category_names: [null],
-    title: null,
-    index: 0,
-  }));
 
-  const [homePageData, setHomePageData] = useState<any>(initialData);
-  const [activeItem, setActiveItem] = useState<any>();
   const [orderedImages, setOrderedImages] = useState<any>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  // Order images
+  const [activeItem, setActiveItem] = useState<any>(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const slideHandler = (index: any, item: any) => {
+    setActiveItem(index);
+  }
+
   useEffect(() => {
     const getPageDataOrdered = async () => {
-      try {
-        const pageData = await getHomePageDataJson();
-        return orderImages(pageData);
-      } catch (error) {
-        console.error("Error fetching ordered images:", error);
-        return [];
-      }
+      const pageData = await getHomePageDataJson();
+      const orderedImages = orderImages(pageData);
+      setOrderedImages(orderedImages);
+      setIsLoading(false);
     };
     if (orderedImages === null) {
-      setOrderedImages(getPageDataOrdered());
+      getPageDataOrdered();
     }
   }, []);
 
-  // Update home page image
-  // useEffect(() => {
-  //   const processImages = async () => {
-  //     try {
-  //       updateImage(0);
-  //     } catch (error) {
-  //       console.error("Error processing images:", error);
-  //     }
-  //   };
-  //   if (orderImages !== null) {
-  //     processImages();
-  //   }
-  // }, [orderedImages]);
-
-  const updateImage = useCallback(async (idx: number) => {
-    try {
-      if (orderedImages) {
-        setIsLoading(true); // Set loading state to true during data fetching
-        const images = await orderedImages;
-        const [imageData] = await processHomePageData([images[idx]]);
-        initialData[idx] = imageData;
-
-        setHomePageData(initialData);
-        setActiveItem(imageData);
-      }
-    } catch (error) {
-      console.error("Error updating image:", error);
-    } finally {
-      setIsLoading(false); // Set loading state to false after data fetching is complete
-      setIsVisible(true); // Triggers fade-in of image 
-    }
-  }, [orderedImages]);
-
-  // TODO: is this needed?
-  // Set initial image
-  useEffect(() => {
-    homePageData && setActiveItem(homePageData[0]);
-    setIsVisible(true); // Triggers fade-in of image on initial render
-  }, [homePageData]);
-
-  // When activeItem changes
-  useEffect(() => {
-    if (activeItem && activeItem.index || activeItem && activeItem.index === 0) {
-      updateImage(activeItem.index);
-    };
-  }, [activeItem, updateImage]); // is updateImage needed here?
-
-
-  // Conditional rendering of Loading Screen
-  if (!homePageData[0]?.img_id) {
-    return <LoadingScreen />;
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.leftBox}>
         <div className={styles.innerBox}>
-          <Buttons data={homePageData} setItem={setActiveItem} activeItem={activeItem} isLoading={isLoading} setIsVisible={setIsVisible} />
+          <Buttons data={orderedImages} setItem={setActiveItem} activeItem={activeItem} />
         </div>
       </div>
       <section id={styles.carousel}>
         <div className={styles.imageContainer}>
-          <CarouselImage activeItem={activeItem} isVisible={isVisible} />
+          <ResponsiveCarousel
+            animationHandler={"fade"}
+            infiniteLoop={true}
+            showArrows={false}
+            showIndicators={false}
+            showThumbs={false}
+            showStatus={false}
+            autoPlay={true}
+            interval={5000}
+            onChange={slideHandler}
+            selectedItem={activeItem}
+          >
+            {orderedImages.map((item: any, index: any) => (
+              <div key={index}>
+                <img className={styles.carouselImage} src={item.img_url} />
+              </div>
+            ))}
+          </ResponsiveCarousel>
         </div>
       </section>
     </div>
