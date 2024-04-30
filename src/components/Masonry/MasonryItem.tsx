0278b0f7b5abe4d05bcc5d8306d09c70d0f5627c
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-04-30 20:25:53 +0100

    Fixed double tap issue on projects list

--- src/components/Masonry/MasonryItem.tsx
+++ src/components/Masonry/MasonryItem.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./styles/MasonryItem.module.scss";
 import { useLanguageContext } from "@/context/LanguageContext";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,8 +11,15 @@ type MasonryItemProps = {
   item: any,
 }
 
+
 export default function MasonryItem({ index, item }: MasonryItemProps) {
   const { language } = useLanguageContext();
+  const router = useRouter();
+
+  const goToProject = (item: any, e: any) => {
+    e.stopPropagation();
+    router.push(`/project/?p=${item.slug}`);
+  }
 
   return item.image_in_list && (
     <div key={index} className={`${styles.masonryItem} ${item.image_size === "horizontal" ? styles.horizontalPoster : ""} ${item.image_size === "vertical" ? styles.verticalPoster : ""}`}>
@@ -21,7 +29,7 @@ export default function MasonryItem({ index, item }: MasonryItemProps) {
         alt={item[`title_${language}`] || ""}
         fill
       />
-      <Link href={`/project/?p=${item.slug}`} className={styles.imageOverlay}>
+      <Link href={`/project/?p=${item.slug}`} className={styles.imageOverlay} onTouchStart={(e) => goToProject(item, e)}>
         <div className={styles.overlayTitle}> {item[`title_${language}`] || ""} </div>
         <section className={styles.overlayBottomText}>
           <div>{Array.isArray(item.categories) && item.categories.length > 0 ? item.categories[0].toLowerCase() : ""}</div>
