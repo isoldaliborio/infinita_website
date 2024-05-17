44852a7e7be7c1838274d79025a6af639299d08e
NoahAldhous <naldhous@hotmail.co.uk>
2024-05-17 18:13:42 +0100

    removed description text from contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,17 +17,6 @@ export default function Contact() {
     <>
       <TitleBanner title={language === "en" ? "contact" : "contato"} />
       <main className={styles.main}>
-        <div className={styles.description}>
-          {language === "en" 
-            ? "Infinita Productions is a production company based in London, UK and Salvador, Brazil. We aim to support projects in both countries, encounraging co-productions and stimulatingg a cultural dialogue. Infinita is also committed to creating original content." 
-            : "Infinita Productions é uma produtora com sede em Londres, Reino Unido e Salvador, Brasil. Pretendemos apoiar projetos em ambos os países, incentivando as coproduções e estimulando o diálogo cultural. A Infinita também está comprometida em criar conteúdo original."
-          }
-        <br/>
-        {language === "en" 
-            ? "Our action areas are Films, Music, Curating and Accounting." 
-            : "As nossas áreas de atuação são Cinema, Música, Curadoria e Contabilidade."
-          }
-        </div>
         <ContactForm />
       </main>
     </>
