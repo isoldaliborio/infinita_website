38f60b241307246f40fef6f3e783e878b777d84b
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-05-26 20:11:35 +0100

    Email sending with emailJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+const variables = {
+    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+}
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
@@ -15,6 +22,7 @@ const nextConfig = {
             }
         ]
     },
+    env: variables,
     reactStrictMode: false,
 };
 
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,12 +3,13 @@
 import styles from "./styles/ContactForm.module.scss";
 import { useState, useEffect } from "react";
 import { useLanguageContext } from "@/context/LanguageContext";
+import emailjs from 'emailjs-com';
 
 export default function ContactForm() {
   const { language } = useLanguageContext();
 
-  // Workaround for LastPass extension. May be removed if we render the page based on some other state
   const [isClient, setIsClient] = useState(false);
+  const [emailStatus, setEmailStatus] = useState<string | null>(null);
 
   useEffect(() => setIsClient(true), []);
 
@@ -20,6 +21,33 @@ export default function ContactForm() {
     const inputName = formData.get('inputName');
     const inputEmail = formData.get('inputEmail');
     const inputMessage = formData.get('message');
+
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    if (!serviceID || !templateID || !publicKey) {
+      console.error('EmailJS environment variables are not properly set.');
+      return;
+    }
+
+    const templateParams = {
+      from_name: inputName,
+      from_email: inputEmail,
+      message: inputMessage
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, publicKey)
+      .then((response) => {
+        if (response.status === 200) {
+          setEmailStatus('success');
+        } else {
+          setEmailStatus('error');
+        }
+      }, (error) => {
+        console.error(error);
+        setEmailStatus('error');
+      });
   };
 
   return (
@@ -27,7 +55,11 @@ export default function ContactForm() {
       <input required className={`${styles.name} ${styles.input}`} type="string" name="inputName" placeholder={language === "en" ? "name" : "nome"} />
       <input required className={`${styles.email} ${styles.input}`} type="string" name="inputEmail" placeholder={language === "en" ? "email" : "e-mail"} />
       <textarea required className={`${styles.message} ${styles.input}`} name="message" placeholder={language === "en" ? "message" : "mensagem"} />
-      <button className={`${styles.submitButton} col-2`} type="submit">{language === "en" ? "send" : "enviar"}</button>
+      {emailStatus !== 'success' && (
+        <button className={`${styles.submitButton} col-2`} type="submit">{language === "en" ? "send" : "enviar"}</button>
+      )}
+      {emailStatus === 'success' && <p>{language === "en" ? "Your message was sent!" : "Seu email foi enviado!"}</p>}
+      {emailStatus === 'error' && <p>{language === "en" ? "There was an error sending your message, try again" : "Erro ao enviar sua mensagem, tente novamente"}</p>}
     </form>
   );
 }
