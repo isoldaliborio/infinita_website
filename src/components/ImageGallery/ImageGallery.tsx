f93ad0eff1e944c42ec7fc0d1f19dbb08e30bc8c
Eduardo Haddad <eduardo.torquemada@gmail.com>
2024-06-08 17:09:14 +0100

    Added image captions

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import "./styles/LightGallery.scss";
 import styles from "./styles/ImageGallery.module.scss";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 
 import type { LightGallery } from "lightgallery/lightgallery";
@@ -23,27 +24,42 @@ type ImageGalleryProps = {
 };
 
 export default function ImageGallery({ galleryImages }: ImageGalleryProps) {
-  function findImageWidth(item: any, targetWidth: any) {
-    //make an array of keys
-    const keyArray = Object.keys(item);
+  const [screenSize, setScreenSize] = useState<string>('large'); // Default to large
 
-    //if target width exists, just use that
+  function findImageWidth(item: any, targetWidth: any) {
+    const keyArray = Object.keys(item);
     if (keyArray.includes(targetWidth)) {
       return targetWidth;
     } else {
-      //if target width doesnt exist, sort through array to find nearest number to it, and use that instead
       const target = parseInt(targetWidth.slice(0, -1));
-
       keyArray.sort((a: any, b: any) => {
         return (
           Math.abs(parseInt(a.slice(0, -1)) - target) -
           Math.abs(parseInt(b.slice(0, -1)) - target)
         );
       })[0];
-
       return keyArray[0];
     }
   }
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 700) {
+        setScreenSize('small');
+      } else if (screenWidth > 700 && screenWidth <= 1169) {
+        setScreenSize('medium');
+      } else {
+        setScreenSize('large');
+      }
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
 
   const lightboxRef = useRef<LightGallery | null>(null);
 
@@ -80,17 +96,20 @@ export default function ImageGallery({ galleryImages }: ImageGalleryProps) {
       <LightGalleryComponent
         onInit={(ref) => {
           if (ref) {
-            lightboxRef.current = ref.instance; //opens the lightbox on the image that was clicked
+            lightboxRef.current = ref.instance;
           }
         }}
-        speed={500} //transition speed
-        download={false} //user cannot download the images
+        speed={500}
+        download={false}
         plugins={[lgThumbnail]}
         dynamic
         dynamicEl={galleryImages.map((item: any) => ({
           src: item[findImageWidth(item, "1536w")],
           thumb: item[findImageWidth(item, "300w")],
+          subHtml: `<div class="lightGallery-captions"><p class="image-caption">${item.caption}</p></div>`,
         }))}
+        appendSubHtmlTo={screenSize === "small" ? ".lg-outer" : ".lg-sub-html"}
+        thumbnail={screenSize !== "small"}
       />
     </>
   );
